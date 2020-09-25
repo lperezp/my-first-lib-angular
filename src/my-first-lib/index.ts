b1900d45513e83c9d058dab9aec28e0772e98f9d
lperezp <luiseperezp.10@gmail.com>
2020-09-24 20:50:12 -0500

    feat(schema): add schema interface

--- src/my-first-lib/index.ts
+++ src/my-first-lib/index.ts
@@ -1,11 +1,20 @@
+import { capitalize } from '@angular-devkit/core/src/utils/strings';
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
+import { join } from 'path';
+
+import { Schema } from './schema';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function myFirstLib(_options: any): Rule {
+export function myFirstLib(_options: Schema): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    tree.create('hello,js',`console.log('rr');`)
+    const name: string = _options.name;
+    const content: string = _options.content;
+    const extension: string = _options.extension || '.md';
+
+    tree.create(join(name, extension), capitalize(content));
+
     return tree;
   };
 }
---
+++ src/my-first-lib/schema.ts
@@ -0,0 +1,5 @@
+export interface Schema {
+  name: string;
+  content: string;
+  extension?: string;
+}
